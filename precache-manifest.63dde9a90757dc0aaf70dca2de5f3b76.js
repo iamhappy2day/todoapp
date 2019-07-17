2b615bd612fc0571a17617e5cbb9f2e3d7554c4d
iamhappy2day <23happyami@gmail.com>
2019-07-17 12:54:54 +0300

    Updates

---
+++ precache-manifest.63dde9a90757dc0aaf70dca2de5f3b76.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9f169010afc4051db73030cbbfc39cb",
+    "url": "/todoApp/todomy/index.html"
+  },
+  {
+    "revision": "c84fdfcdab7cbd1afeaa",
+    "url": "/todoApp/todomy/static/css/main.833ff218.chunk.css"
+  },
+  {
+    "revision": "06f44d7c6eeee966ea35",
+    "url": "/todoApp/todomy/static/js/2.34ad89f7.chunk.js"
+  },
+  {
+    "revision": "c84fdfcdab7cbd1afeaa",
+    "url": "/todoApp/todomy/static/js/main.f59bc7e1.chunk.js"
+  },
+  {
+    "revision": "26d4276d8419aebbc709",
+    "url": "/todoApp/todomy/static/js/runtime~main.e21e75b5.js"
+  }
+]);
